001fd47e4d1fb5e22ea59368d5f0ad93dc9447bc
zhanghaijun666 <zhanghaijun_java@163.com>
2019-01-20 14:47:39 +0800

    'bulid'

--- BlogCloudServer/src/test/java/com/MyResourceTest.java
+++ BlogCloudServer/src/test/java/com/MyResourceTest.java
@@ -1,5 +1,6 @@
 package com;
 
+import com.server.ServerMain;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
@@ -19,17 +20,17 @@ public class MyResourceTest {
     @Before
     public void setUp() throws Exception {
         // start the server
-        server = Main.startServer();
+        server = ServerMain.startServer();
         // create the client
         Client c = ClientBuilder.newClient();
 
         // uncomment the following line if you want to enable
         // support for JSON in the client (you also have to uncomment
-        // dependency on jersey-media-json module in pom.xml and Main.startServer())
+        // dependency on jersey-media-json module in pom.xml and ServerMain.startServer())
         // --
         // c.configuration().enable(new org.glassfish.jersey.media.json.JsonJaxbFeature());
 
-        target = c.target(Main.BASE_URI);
+        target = c.target(ServerMain.BASE_URI);
     }
 
     @After
@@ -42,7 +43,7 @@ public class MyResourceTest {
      */
     @Test
     public void testGetIt() {
-        String responseMsg = target.path("myresource").request().get(String.class);
+        String responseMsg = target.path("statice").request().get(String.class);
         assertEquals("Got it!", responseMsg);
     }
 }
