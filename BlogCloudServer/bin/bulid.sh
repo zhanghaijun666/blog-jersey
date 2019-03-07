4176157172acdeb76bdec5cbd89c1889248e8070
zhanghaijun <zhanghaijun@bjtxra.com>
2019-03-07 09:45:40 +0800

    项目部署脚本

--- BlogCloudServer/bin/bulid.sh
+++ BlogCloudServer/bin/bulid.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+rm -rf /opt/blog
 if [[ ! -d "/opt/BlogCloudJersey" ]]; then
 	cd /opt
 	git clone git@github.com:zhanghaijun666/BlogCloudJersey.git
@@ -13,11 +14,13 @@ if [[ ! -d "/opt/blog" ]]; then
 	mkdir /opt/blog
 fi
 cd /opt/blog
-rm -rf ./*
-cp /opt/BlogCloudJersey/BlogCloudServer/target/BlogCloudServer-1.0-SNAPSHOT.jar /opt/blog/Blog-1.0.jar
-cp -r /opt/BlogCloudJersey/BlogCloudClient/www /opt/blog/www
-cp -r /opt/BlogCloudJersey/BlogCloudServer/config /opt/blog/config
-cp -r /opt/BlogCloudJersey/BlogCloudServer/user.txt /opt/blog/user.txt
-cp -r /opt/BlogCloudJersey/BlogCloudServer/bin /opt/blog/bin
+ln -s /opt/BlogCloudJersey/BlogCloudServer/target/BlogCloudServer-1.0-SNAPSHOT.jar /opt/blog/Blog-1.0.jar
+ln -s /opt/BlogCloudJersey/BlogCloudClient/www /opt/blog/www
+ln -s /opt/BlogCloudJersey/BlogCloudServer/config /opt/blog/config
+ln -s /opt/BlogCloudJersey/BlogCloudServer/user.txt /opt/blog/user.txt
+ln -s /opt/BlogCloudJersey/BlogCloudServer/bin /opt/blog/bin
 chmod -R +x /opt/blog/bin
+if [[ ! -f "/usr/lib/systemd/system/blog.service" ]]; then
+	cp /opt/blog/bin/blog.service /usr/lib/systemd/system/blog.service
+fi
 
--- BlogCloudServer/bin/run.sh
+++ BlogCloudServer/bin/run.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 if [[ -f "/opt/blog/Blog-1.0.jar" ]]; then
+	cd /opt/blog
 	OPTS="-Dsun.jnu.encoding=UTF-8 -Dfile.encoding=UTF-8"
 	java -jar /opt/blog/Blog-1.0.jar com.server.ServerMain
 fi
