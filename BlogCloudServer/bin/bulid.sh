cfe686bdd9fe758b47934934950cc654aba77306
zhanghaijun <zhanghaijun@bjtxra.com>
2019-03-07 16:24:19 +0800

    项目部署脚本

--- BlogCloudServer/bin/bulid.sh
+++ BlogCloudServer/bin/bulid.sh
@@ -16,11 +16,14 @@ fi
 cd /opt/blog
 ln -s /opt/BlogCloudJersey/BlogCloudServer/target/BlogCloudServer-1.0-SNAPSHOT.jar /opt/blog/Blog-1.0.jar
 ln -s /opt/BlogCloudJersey/BlogCloudClient/www /opt/blog/www
-ln -s /opt/BlogCloudJersey/BlogCloudServer/config /opt/blog/config
-ln -s /opt/BlogCloudJersey/BlogCloudServer/user.txt /opt/blog/user.txt
-ln -s /opt/BlogCloudJersey/BlogCloudServer/bin /opt/blog/bin
-chmod -R +x /opt/blog/bin
+cp -r /opt/BlogCloudJersey/BlogCloudServer/config /opt/blog/config
+cp /opt/BlogCloudJersey/BlogCloudServer/user.txt /opt/blog/user.txt
+cp -r /opt/BlogCloudJersey/BlogCloudServer/bin /opt/blog/bin
+chmod -R +x /opt/blog/bin/*.sh
+if [[ ! -d "/usr/lib/systemd/system" ]]; then
+	mkdir -p /usr/lib/systemd/system
+fi
 if [[ ! -f "/usr/lib/systemd/system/blog.service" ]]; then
 	cp /opt/blog/bin/blog.service /usr/lib/systemd/system/blog.service
 fi
-
+#systemctl start blog
