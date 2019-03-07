0af96d2981d711c6012b4fbffc7754cf216cff46
zhanghaijun666 <zhanghaijun_java@163.com>
2019-03-07 20:59:32 +0800

    bulid

---
+++ BlogCloudServer/bin/docker_mysql.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+if [[ ! -z $(docker ps | grep blog_mysql) ]]; then
+	echo "docker_mysql started"
+elif [[ -z $(docker ps -a | grep blog_mysql) ]]; then
+	echo "docker_mysql run"
+	docker run --name blog_mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=1q2w3e -d mysql
+else
+	echo "docker_mysql starting"
+	docker start blog_mysql
+fi
+
+#进入容器
+#docker exec -it blog_mysql bash
