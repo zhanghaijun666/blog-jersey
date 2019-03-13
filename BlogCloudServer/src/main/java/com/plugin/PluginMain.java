a3f75e673d5ac36f599f3a98123e0acd275539ce
zhanghaijun <zhanghaijun@bjtxra.com>
2019-03-13 17:24:47 +0800

    bulid

---
+++ BlogCloudServer/src/main/java/com/plugin/PluginMain.java
@@ -0,0 +1,14 @@
+package com.plugin;
+
+
+/**
+ * @author zhanghaijun
+ */
+public class PluginMain {
+
+//    public static void main(String[] args) {
+//        org.pf4j.PluginManager pluginManager = new org.pf4j.DefaultPluginManager();
+//        pluginManager.loadPlugins();
+//        pluginManager.startPlugins();
+//    }
+}
