b521ed4217ac5e9e770c6acbb5150cf2d8920d5a
zhanghaijun666 <zhanghaijun_java@163.com>
2019-03-14 23:05:54 +0800

    bulid

--- BlogCloudClient/www/user/js/util/uploadFile.js
+++ BlogCloudClient/www/user/js/util/uploadFile.js
@@ -10,4 +10,32 @@
         });
 
     }, 1000);
+
+
+    function uploadFile(url, file) {
+        var xhr = createBinary(url, file);
+        //file = document.getElementById("file").files[0];
+        var form = new FormData();
+        form.append("file", file);
+        xhr.send(form);
+        
+        
+    }
+    function createBinary(url) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("post", url, true);
+        xhr.onload = function () {
+            console.log("上传成功！");
+        };
+        xhr.onerror = function () {
+            console.log("上传失败！");
+        };
+        xhr.upload.onprogress = function(evt){
+            if (evt.lengthComputable) {
+                var percentComplete = Math.round(evt.loaded * 100 / evt.total);
+                console.log(percentComplete);
+            }
+        };
+        return xhr;
+    }
 })();
