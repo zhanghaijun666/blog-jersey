81530b4ddb7976faca280894dcdf2de66a752caa
zhanghaijun666 <zhanghaijun_java@163.com>
2019-03-24 01:30:00 +0800

    bulid

--- BlogCloudServer/bin/run.sh
+++ BlogCloudServer/bin/run.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
 
+echo "export JAVA_HOME=/usr/local/tools/java_jdk" >> /etc/profile
+echo "export PATH=\$PATH:\$JAVA_HOME/bin" >> /etc/profile
+source /etc/profile
+
 if [[ -f "/opt/Blog/Blog-1.0.jar" ]]; then
 	cd /opt/Blog
 	OPTS="-Dsun.jnu.encoding=UTF-8 -Dfile.encoding=UTF-8"
-	java -jar /opt/Blog/Blog-1.0.jar com.server.ServerMain
+	java -jar /opt/Blog/Blog-1.0.jar com.server.ServerMain > /opt/blog-service.log
 	#java -cp lib/*:/opt/Blog/Blog-1.0.jar com.server.ServerMain initdb "$@"
 fi
