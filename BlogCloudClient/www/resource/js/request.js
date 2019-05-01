0a2d560aaa76d9f5850cd8e8c370772b95e04901
zhanghaijun666 <zhanghaijun_java@163.com>
2019-05-01 17:29:12 +0800

    bulid

--- BlogCloudClient/www/resource/js/request.js
+++ BlogCloudClient/www/resource/js/request.js
@@ -1,67 +1,67 @@
-const server = window.location.protocol + "//" + window.location.host;
-
-function isDebug() {
-    return /^(localhost|127\.0\.0\.1)/.test(window.location.host);
-}
-function getServerUrl(url) {
-    if (url.startsWith("/")) {
-        url = server + url;
-    } else {
-        url = server + "/" + url;
-    }
-    return url;
-}
-
-function getRequest(url, options, callback, erroCallback) {
-    var requestValue = {
-        method: "GET",
-        async: true,
-        accept: "text/plain",
-        timeout: isDebug() ? 0 : 2000,
-        withCredentials: true,
-        "Content-Type": "application/json; charset=UTF-8"
+(function (exports) {
+    exports.blogServer = window.location.protocol + "//" + window.location.host;
+    exports.isDebug = function () {
+        return /^(localhost|127\.0\.0\.1)/.test(window.location.host);
     };
-    options = $.extend({}, requestValue, options);
-    var xhr = createXMLHttpRequest(options, callback, erroCallback);
-    xhr.open(options.method, url, options.async);
-    xhr.timeout = options.timeout;
-    xhr.setRequestHeader("Accept", options.accept);
-    xhr.responseType = options.responseType || options.accept.indexOf("text/") > -1 ? "text" : "arraybuffer";
+    exports.getServerUrl = function (url) {
+        if (url.startsWith("/")) {
+            url = blogServer + url;
+        } else {
+            url = blogServer + "/" + url;
+        }
+        return url;
+    };
+    exports.getRequest = function (url, options, callback, erroCallback) {
+        var requestValue = {
+            method: "GET",
+            async: true,
+            accept: "text/plain",
+            timeout: isDebug() ? 0 : 2000,
+            withCredentials: true,
+            "Content-Type": "application/json; charset=UTF-8"
+        };
+        options = $.extend({}, requestValue, options);
+        var xhr = createXMLHttpRequest(options, callback, erroCallback);
+        xhr.open(options.method, url, options.async);
+        xhr.timeout = options.timeout;
+        xhr.setRequestHeader("Accept", options.accept);
+        xhr.responseType = options.responseType || options.accept.indexOf("text/") > -1 ? "text" : "arraybuffer";
 
-    if (options.data) {
-        xhr.setRequestHeader("Content-Type", options["Content-Type"]);
-        xhr.send(options.data);
-    } else if (options.formdata) {
-        var form = new FormData();
-        form.append("file", options.formdata);
-        xhr.send(form);
-    } else {
-        xhr.send();
-    }
-}
-function createXMLHttpRequest(options, callback, erroCallback) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                let result;
-                if (xhr.responseType === 'text') {
-                    result = xhr.responseText;
-                } else if (xhr.responseType === 'document') {
-                    result = xhr.responseXML;
-                } else if (xhr.responseType === "arraybuffer") {
-                    result = new Uint8Array(xhr.response);
-                } else {
-                    result = xhr.response;
-                }
-                if (callback) {
-                    callback(result, xhr);
-                }
-            } else if (erroCallback) {
-                erroCallback(xhr);
-            }
+        if (options.data) {
+            xhr.setRequestHeader("Content-Type", options["Content-Type"]);
+            xhr.send(options.data);
+        } else if (options.formdata) {
+            var form = new FormData();
+            form.append("file", options.formdata);
+            xhr.send(form);
+        } else {
+            xhr.send();
         }
     };
+
+    function createXMLHttpRequest(options, callback, erroCallback) {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    let result;
+                    if (xhr.responseType === 'text') {
+                        result = xhr.responseText;
+                    } else if (xhr.responseType === 'document') {
+                        result = xhr.responseXML;
+                    } else if (xhr.responseType === "arraybuffer") {
+                        result = new Uint8Array(xhr.response);
+                    } else {
+                        result = xhr.response;
+                    }
+                    if (callback) {
+                        callback(result, xhr);
+                    }
+                } else if (erroCallback) {
+                    erroCallback(xhr);
+                }
+            }
+        };
 //    xhr.upload.onprogress = function (e) {
 //        if (e.lengthComputable) {
 //            var percentage = (e.loaded / e.total) * 100;
@@ -85,5 +85,6 @@ function createXMLHttpRequest(options, callback, erroCallback) {
 //    xhr.ontimeout = e => {
 //        console.log('request timeout');
 //    };
-    return xhr;
-}
+        return xhr;
+    }
+})(this);
